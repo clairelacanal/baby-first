f2c8abaee1c1d7a02a4c9a1b430bd762a3576fbd
Claire Lacanal <lacanal.claire@gmail.com>
2021-01-14 17:01:17 +0100

    file auth-service done

---
+++ client/src/components/Auth/auth-service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const service = axios.create({
+  baseURL: `${process.env.REACT_APP_APIURL || ""}`,
+  withCredentials: true
+});
+export default service;
+
+function signup(username, email, password) {
+  return service.post('/auth/signup', {username, email, password}).then(response => response.data)
+}
+export {signup}
+
+function loggedin() {
+  return service.get('/auth/loggedin').then(response => response.data)
+}
+export {loggedin}
+
+function login(username, password) {
+  return service.post('/auth/login', {username, password}).then(response => response.data)
+}
+export {login}
+
+function logout() {
+  return service.post('/auth/logout', {}).then(response => response.data)
+}
+export {logout}
+
+function upload(formdata) {
+  return service.post('/upload', formdata).then(response => response.data)
+}
+export {upload}
+
