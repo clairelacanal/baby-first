aae5fb88852c11a3ab09d4ce1bf9c50d0a2e37b6
Claire Lacanal <lacanal.claire@gmail.com>
2021-01-15 14:47:34 +0100

    Navbar file initialisation

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import {logout} from '../Auth/auth-service';
+import './Navbar.css';
+
+
+class Navbar extends React.Component {
+    render(){
+        return()
+    }
+}
+
+export default Navbar;
