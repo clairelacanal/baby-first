4f70b03b8cbd309e405d682ad5b1e3253b569a68
Claire Lacanal <lacanal.claire@gmail.com>
2021-01-26 15:46:21 +0100

    profile page on going

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Redirect} from 'react-router-dom';
+import './Profile.css';
+
+class Profile extends React.Component {
+    state = {
+        user : {}
+    }
+
+
+}
+
+export default Profile;
