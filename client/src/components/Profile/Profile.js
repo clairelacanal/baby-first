763e80865fafd7b6dd14da99278c276db048fefa
Claire Lacanal <lacanal.claire@gmail.com>
2021-01-27 10:36:25 +0100

    profile on going v2

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import service from '../Auth/auth-service';
 import {Redirect} from 'react-router-dom';
 import './Profile.css';
 
 class Profile extends React.Component {
     state = {
         user : {}
+    }
+
+    render(){
+    
+    getUserProfile = () => {
+        const params = this.props.match.params;
+        .then(responseFromApi => {
+            const theUser = params.id;
+            this.setState({user: theUser});
+        })
+        .catch(err => {
+            console.log(`Le user profile n'est pas le bon`);
+        })
+    }
+        return(
+
+        )
     }
 
 
