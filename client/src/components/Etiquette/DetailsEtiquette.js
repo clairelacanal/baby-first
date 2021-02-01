78c1db90b39f733010a8f627337a4f854ec2cc13
Claire Lacanal <lacanal.claire@gmail.com>
2021-02-01 17:35:36 +0100

    function getOneEtiquette done

--- client/src/components/Etiquette/DetailsEtiquette.js
+++ client/src/components/Etiquette/DetailsEtiquette.js
@@ -1,9 +1,24 @@
 import React from 'React';
+import service from '../Auth/auth-service';
 import './Etiquette.css';
 
 class DetailsEtiquette extends React.Component{
     state = {
         etiquette : {}
+    }
+
+    //Function get0neEtiquette
+
+    getOneEtiquette = () => {
+        const {params} = this.props.match
+        service.get(`/etiquette/${params.id}`)
+        .then(responseFromApi => {
+            const theEtiquette = responseFromApi.data;
+            this.setState({etiquette: theEtiquette})
+        })
+        .catch((err) => {
+            console.log('erreur pour charger', err)
+        })
     }
 
     render(){
