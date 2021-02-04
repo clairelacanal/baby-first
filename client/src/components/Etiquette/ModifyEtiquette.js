9ac2bd2f6c3add63c488712b60c797e8505851cf
Claire Lacanal <lacanal.claire@gmail.com>
2021-02-04 19:08:08 +0100

    handleSubmit modidy etiquette on going

--- client/src/components/Etiquette/ModifyEtiquette.js
+++ client/src/components/Etiquette/ModifyEtiquette.js
@@ -4,6 +4,36 @@ import service from '../Auth/auth-service';
 import './Etiquette.css';
 
 class ModifyEtiquette extends React.Component {
+    state = {
+        etiquette : {
+            title: '',
+            lieu: '',
+            date: new Date(),
+            imageUrl: '',
+            commentaire: '',
+            redirect: false
+        }
+    }
+
+    //Function handleSubmit() pour soumettre le formulaire
+    handleSubmit = (event) => {
+        preventDefault(event);
+        const title = this.state.title,
+        const lieu = this.state.lieu,
+        const date = this.state.date,
+        const imageUrl = this.state.imageUrl,
+        const commentaire = this.state.commentaire
+
+        service.post('/etiquette', {title, lieu, date, imageUrl, commentaire})
+        .then(() => {
+            this.setState({title, lieu, date, imageUrl, redirect:true});
+        })
+        .catch(err => console.log(err));
+    }
+
+    //function handleChange pour changer l'ancienne valeur en nouvelle valeur
+    
+
     render(){
         return(
             <div id="section-modify-etiquette">
