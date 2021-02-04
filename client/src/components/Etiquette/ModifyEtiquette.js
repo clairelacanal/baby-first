85ff1f0148e5ff24c2008d0ed6faaca0077ed556
Claire Lacanal <lacanal.claire@gmail.com>
2021-02-04 14:27:41 +0100

    ModifyEtiquette on going

---
+++ client/src/components/Etiquette/ModifyEtiquette.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import service from '../Auth/auth-service';
+import './Etiquette.css';
+
+class ModifyEtiquette extends React.Component {
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default ModifyEtiquette;
