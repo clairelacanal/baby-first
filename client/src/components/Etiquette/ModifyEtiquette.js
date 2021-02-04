c4f40a3157c2d2681cf724f59bc9b0870cd755fe
Claire Lacanal <lacanal.claire@gmail.com>
2021-02-04 14:46:31 +0100

    modify etiquette v1

--- client/src/components/Etiquette/ModifyEtiquette.js
+++ client/src/components/Etiquette/ModifyEtiquette.js
@@ -6,7 +6,22 @@ import './Etiquette.css';
 class ModifyEtiquette extends React.Component {
     render(){
         return(
+            <div id="section-modify-etiquette">
+                <div className="container-modify-etiquette">
+                    <div className="image-modify-etiquette">
 
+                    </div>
+                    <div className="formulaire-modify-etiquette">
+                        <form className="form-modify-etiquette">
+                            <label>Titre:</label>
+                            <label>Lieu:</label>
+                            <label>Date:</label>
+                            <label>Commentaire:</label>
+                            <Link to="/profile">Validez les modifications</Link>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
