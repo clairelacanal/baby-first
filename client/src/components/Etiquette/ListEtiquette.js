4d76c9d14fc6ba70f3ac17876315a0531359b948
Claire Lacanal <lacanal.claire@gmail.com>
2021-02-11 15:38:53 +0100

    listOfEtiquettes on going

--- client/src/components/Etiquette/ListEtiquette.js
+++ client/src/components/Etiquette/ListEtiquette.js
@@ -3,9 +3,13 @@ import './Etiquette.css';
 
 class ListEtiquette extends React.Component {
     state = {
-
+        listOfEtiquettes = []
     }
+
     render(){
+        if(this.state.listOfEtiquettes.length === 0 ) {
+            return "Tu n'as pas encore d'étiquette"
+        }
         return()
     }
 }
