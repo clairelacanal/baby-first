eadebdac249d2d2d245f8155fcc3a6e15bbe7c33
Claire Lacanal <lacanal.claire@gmail.com>
2021-02-25 21:06:34 +0100

    modify done

--- client/src/components/Etiquette/ModifyEtiquette.js
+++ client/src/components/Etiquette/ModifyEtiquette.js
@@ -5,7 +5,7 @@ import './Etiquette.css';
 
 class ModifyEtiquette extends React.Component {
     state = {
-        etiquette : null
+        etiquette : {}
     }
 
      //Function get0neEtiquette
@@ -63,37 +63,43 @@ class ModifyEtiquette extends React.Component {
         let inputName = element.target.name;
         let inputValue = element.target.value;
 
-        let newEtiquette = Object.assign({},this.state);
-        newEtiquette.etiquette[inputName].value = inputValue;
+        let newEtiquette = Object.assign({}, this.state.etiquette);
+        newEtiquette[inputName] = inputValue;
+        this.setState({etiquette : newEtiquette})
     }
 
     render(){
+        if (this.state.etiquette == null) {
+            return null;
+        } else {
+
         return(
             <div id="section-modify-etiquette">
                 <div className="container-modify-etiquette">
                     <div className="formulaire-modify-etiquette">
                         <form onSubmit={this.handleFormSubmit} className="formulaire-modify-etiquette">
                         <label>Changez le titre:</label>
-                            <input type="text" name="title" value={this.state.etiquette.title} onChange={event => this.handleChange(event)}/>
+                            <input type="text" name="title" value={this.state.etiquette.title} onChange={element => this.handleChange(element)}/>
 
                             <label>Changez le lieu:</label>
-                            <input type="text" name="lieu" value={this.state.etiquette.lieu} onChange={event => this.handleChange(event)}/>
+                            <input type="text" name="lieu" value={this.state.etiquette.lieu} onChange={element => this.handleChange(element)}/>
 
                             <label>Changez la date:</label>
-                            <input type="date" name="date" value={this.state.etiquette.date} onChange={event => this.handleChange(event)}/>
+                            <input type="date" name="date" value={this.state.etiquette.date} onChange={element => this.handleChange(element)}/>
 
                             <label>Changez l'image:</label>
-                            <input type="file" name="imageUrl" value={this.state.etiquette.imageUrl} onChange={event => this.handleChange(event)}/>
+                            <input type="file" name="imageUrl" value={this.state.etiquette.imageUrl} onChange={element => this.handleChange(element)}/>
 
                             <label>Changez le commentaire:</label>
-                            <textarea name="commentaire" value={this.state.etiquette.commentaire} onChange={event => this.handleChange(event)}/>
+                            <textarea name="commentaire" value={this.state.etiquette.commentaire} onChange={element => this.handleChange(element)}/>
 
                             <button>Mettre à jour les modifications</button>
                         </form>
                     </div>
                 </div>
             </div>
-        )
+            )
+        }
     }
 }
 
