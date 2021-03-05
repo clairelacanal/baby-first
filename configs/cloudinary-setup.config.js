21f17bb6684994e9957fcde48f20ee3fe05ef8eb
Claire Lacanal <lacanal.claire@gmail.com>
2021-03-05 17:59:06 +0100

    cloudinary pb resolu

--- configs/cloudinary-setup.config.js
+++ configs/cloudinary-setup.config.js
@@ -10,11 +10,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  folder: 'baby-first', // The name of the folder in cloudinary
-  allowedFormats: ['jpg', 'png'],
-  // params: { resource_type: 'raw' }, => this is in case you want to upload other type of files, not just images
-  filename: function (req, res, cb) {
-    cb(null, res.originalname); // The file on cloudinary would have the same name as the original file name
+  params: {
+    folder: 'baby-first',
+    allowedFormats: ['jpg', 'png'],
+    public_id: (req, file) => file.originalname
   }
 });
 
