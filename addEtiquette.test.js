1941cc19c818ec6034295640ba98ba303564f29e
Claire Lacanal <lacanal.claire@gmail.com>
2021-03-22 17:23:04 +0100

    mocha tests on going

---
+++ addEtiquette.test.js
@@ -0,0 +1,8 @@
+const AddEtiquette = require('./AddEtiquette');
+const assert = require('assert').strict;
+
+/*describe([String with Test Group Name], function() {
+  it([String with Test Name], function() {
+      [Test Code]
+  });
+});*/
---
+++ modifyEtiquette.test.js
@@ -0,0 +1,10 @@
+const ModifyEtiquette = require('./ModifyEtiquette');
+const assert = require('assert').strict;
+const fs = require('fs');
+
+/*describe([String with Test Group Name], function() {
+  it([String with Test Name], function() {
+      [Test Code]
+  });
+});*/
+
